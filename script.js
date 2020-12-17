6e3465e651eb4c56d914b156b3eabf5689141791
maggiebacon <73251619+maggiebacon@users.noreply.github.com>
2020-12-17 13:03:49 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function options() {
+   location.replace("options.html")
+  }
+
+function secondfloor()  {
+        location.replace("2ndFloor.html")
+}
+function secondfloor()  {
+        location.replace("2ndFloor.html")
+}
